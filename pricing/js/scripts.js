b6a89ff136436e9902000378b8236b2263fdc267
djimenezchocron <117752348+djimenezweb@users.noreply.github.com>
2023-01-08 18:35:14 +0100

    Arreglados algunos scripts

--- pricing/js/scripts.js
+++ pricing/js/scripts.js
@@ -2,12 +2,17 @@ const iconElement = document.getElementById('menu-icon')
 const navElement = document.getElementById('navigation')
 
 const toggleButtonElement = document.getElementById('toggle-button')
+const pricingContainerElement = document.getElementById('pricing-container')
 
 iconElement.addEventListener('click', () => {
     if (navElement.classList.contains('nav--show')) {
-        iconElement.src = "../../assets/shared/mobile/menu.svg"
+        iconElement.src = "../assets/shared/mobile/menu.svg"
     } else {
-        iconElement.src = "../../assets/shared/mobile/close.svg"
+        iconElement.src = "../assets/shared/mobile/close.svg"
     }
     navElement.classList.toggle('nav--show')
+})
+
+toggleButtonElement.addEventListener('click', () => {
+    pricingContainerElement.classList.toggle('yearly');
 })
